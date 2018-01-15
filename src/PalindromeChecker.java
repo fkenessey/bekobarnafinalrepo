24c98c6988567efadbf5e0ec46e820f20d5826dd
bekobarna <bocce15@gmail.com>
2018-01-15 09:45:14 +0100

    palindromeChecker done

--- src/PalindromeChecker.java
+++ src/PalindromeChecker.java
@@ -5,17 +5,19 @@
 public class PalindromeChecker {
 
     public static void main(String[] args) {
-        String word1 = "anna";
+        String word1 = "ana";
         String word2 = "racecar";
         String word3 = "noon";
         String word4 = "level";
         String word5 = "hello";
+        String word6 = "palindrome";
 
         System.out.println("is the word " + word1 + " a palindrome?  " + palindromeChecker(word1));
         System.out.println("is the word " + word2 + " a palindrome?  " + palindromeChecker(word2));
         System.out.println("is the word " + word3 + " a palindrome?  " + palindromeChecker(word3));
         System.out.println("is the word " + word4 + " a palindrome?  " + palindromeChecker(word4));
         System.out.println("is the word " + word5 + " a palindrome?  " + palindromeChecker(word5));
+        System.out.println("is the word " + word6 + " a palindrome?  " + palindromeChecker(word6));
     }
 
     public static boolean palindromeChecker(String inputWord) {
